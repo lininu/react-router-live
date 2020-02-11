6cbd9820bc02d304185f617a19dba0442c8b013c
Inu Lin <inuworks@gmail.com>
2020-02-11 12:13:27 +0800

    Updates

--- precache-manifest.00e6dd74891ddd30b849cef5d179057f.js
+++ precache-manifest.00e6dd74891ddd30b849cef5d179057f.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-router-live/static/js/runtime~main.ffaa0a9c.js"
   },
   {
-    "revision": "cf7d232ceab9cd682593",
-    "url": "/react-router-live/static/js/main.fba90673.chunk.js"
+    "revision": "93205722695fecf72f3a",
+    "url": "/react-router-live/static/js/main.81f603ec.chunk.js"
   },
   {
     "revision": "c60fa686a07f5b7389c1",
     "url": "/react-router-live/static/js/2.ca4fedf9.chunk.js"
   },
   {
-    "revision": "cf7d232ceab9cd682593",
+    "revision": "93205722695fecf72f3a",
     "url": "/react-router-live/static/css/main.9effc8e0.chunk.css"
   },
   {
-    "revision": "c7b18b06f3c20faba24e6948d9cdaa61",
+    "revision": "ce8b4c76d4d63230f71a3ffcf37eea7c",
     "url": "/react-router-live/index.html"
   }
 ];
